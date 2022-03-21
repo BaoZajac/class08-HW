816e4c1b4048c2c52c281e35f447ae07926a6879
Basia <bao.zajac@gmail.com>
2022-03-21 16:40:25 +0100

    dodanie poprawnego wyswietlania na ekranie

--- reader.py
+++ reader.py
@@ -18,25 +18,25 @@ plik_wejsciowy = sys.argv[1]
 
 # otwieranie pliku csv
 lista = []
-print("wersja pierwotna:")
 with open(plik_wejsciowy, "r") as f:
     reader = csv.reader(f)
     for line in reader:
         lista.append(line)
-
-print(lista)
+# print(lista)
 
 # modyfikacja pliku csv
-print("modyfikacja:")
 for idx in sys.argv[3:]:
     Y, X, wartosc = idx.split(",")
     lista[int(Y)][int(X)] = wartosc
 
-print(lista)
+# wyświetlenie na ekranie
+for element in lista:
+    print(','.join(element))
 
 # zapisanie do pliku
 plik_exit = sys.argv[1]
 with open("plik.csv", "w") as f:
     writer = csv.writer(f)
     for linia in lista:
-        writer.writerow(linia)
+        writer.writerow(linia)   #TODO: jak pozbyć się tych zbędnych enterów wewnątrz pliku?
+
